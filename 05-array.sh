42f5d644da59b7e4cfcfd8171ee51f24245a91ee
Ratnaraju <ratnaraju.j222@gmail.com>
2024-08-28 19:49:14 +0530

    adding variables

--- 05-array.sh
+++ 05-array.sh
@@ -4,5 +4,5 @@
 #index starts from 0
 
 fruits=("apple" "Orange" "banana")
-echo "first fruit is $fruits{array[0]}"
-echo "first fruit is $fruits{array[@]}"
+echo "first fruit is ${fruits[0]}"
+echo "first fruit is ${fruits[@]}"
