66083ed8796d2c80bd4dd278c1a0981bd4405509
Ratnaraju <ratnaraju.j222@gmail.com>
2024-09-01 22:50:17 +0530

    addition

--- 07-numbersadding.sh
+++ 07-numbersadding.sh
@@ -2,8 +2,9 @@
 number1=$1
 number2=$2
 
-echo first number is "$1"
-echo second number is "$2"
+echo "first number is $1"
+echo "seconf number is $2"
 
-Sum=$(($number1+$number2 ));
-echo total is : $sum;
+sum=$(($number1+$number2))
+
+echo total is $sum;
