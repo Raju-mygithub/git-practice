c4ee6b3f48b02afe74b7762df58f85376477b33f
Ratnaraju <ratnaraju.j222@gmail.com>
2024-09-02 15:44:32 +0530

    functions-1

--- 10-functions.sh
+++ 10-functions.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 userid=$(id -u)
 echo "executed user id is $userid";
+validation[]
+{
+
+if [ $1 -ne 0 ]
+then echo "executed command is true package is not these for  $2"
+else command is not suceess means package for  $2 "already there"
+}
+
+dnf list installed git
+if [ $? -ne 0 ]
+then 
+validation $? "git installation"
+echo "git package need to be install"
+else 
+echo "package already there"
+exit 1;
+
+dnf install git
+
+validation $? "git package"
+echo "package installation is success"
+else
+echo "package already there";
+
+
+
