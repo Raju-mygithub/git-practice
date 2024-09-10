682b9bacfc3cb509c5606a315777c049ee9d9206
Ratnaraju <ratnaraju.j222@gmail.com>
2024-09-10 20:26:15 +0530

    redirectors

--- 12redirections.sh
+++ 12redirections.sh
@@ -1,15 +1,15 @@
 #!/bin/bash/
 
 LOGS_FOLDER="/var/log/shell-scripts"
-SCRIPT_NAME=$($0 | cut -d '.' -f1)
-TIME_STAMP=$date
-#LOG_FILE=$($LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log)
-mkdir -p "/var/log/shell-scripts"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
+mkdir -p $LOGS_FOLDER
 
 userid=$(id -u)
 
 CHECK_ROOT(){
-    if ($userid -ne 0)
+    if [ $userid -ne 0 ]
 
     then
     echo "user doesnot have root permissions, please login with root"
@@ -19,7 +19,7 @@ CHECK_ROOT(){
 }
 
 VALIDATE(){
-    if($1 -ne 0)
+    if [ $1 -ne 0 ]
     then 
     echo "$2 $package failed,please check"
     exit 1
@@ -29,23 +29,23 @@ VALIDATE(){
 
 }
 
-CHECK_ROOT()
-
 CHECK_USAGE(){
 
     echo "usage of script like scriptname and give arguments"
 }
+CHECK_ROOT
 
-if ( $# -eq 0 )
+
+if [ $# -eq 0 ]
 then
 CHECK_USAGE
-exit 1
+
 fi
 
 for package in $@
 do 
 dnf list installed $package
-if ( $? -ne 0 )
+if [ $? -ne 0 ]
 then 
 echo "$package not listed ,please installit"
 
@@ -53,7 +53,7 @@ dnf install $package -y
 VALIDATE $? "installed package for $package"
 else
 echo "$package already there , nothing do"
-exit 1
+
 fi
 done
 
